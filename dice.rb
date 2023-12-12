a28b414667f7ac04ca58e8ed7efebaace1430d88
Sera Oraha <72953661+sera-11@users.noreply.github.com>
2023-12-12 22:44:33 +0000

    added all links

--- dice.rb
+++ dice.rb
@@ -9,9 +9,17 @@ BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  "Dice Roll"
+  "
+  <h1>Dice Roll</h1>
 
-  <a href="/dice/2/6">2d6</a>
+  <ul>
+    <li> <a href='/dice/2/6'>Roll two 6-sided dice</a> </li>
+    <li> <a href='/dice/2/10'>Roll two 10-sided dice</a> </li>
+    <li> <a href='/dice/1/20'>Roll one 20-sided die</a> </li>
+    <li> <a href='/dice/5/4'>Roll five 4-sided dice</a> </li>
+  </ul>
+
+  "
 end
 
 get("/zebra") do
